b1b5b72b4ac5dea0ed739c7d2e4d8faf34f4e3eb
saraford <saraford@users.noreply.github.com>
2016-10-05 12:41:17 -0700

    init files commit

---
+++ ForkTeset1_5Oct2016/ForkTeset1_5Oct2016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkTeset1_5Oct2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
